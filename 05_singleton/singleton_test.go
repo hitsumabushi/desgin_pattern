92a3d49d754f5a192b158eb2e997c03f6c4a63ae
hitsumabushi <053023.math@gmail.com>
2016-01-02 19:52:33 +0900

    Add singleton pattern.

--- 05_singleton/singleton_test.go
+++ 05_singleton/singleton_test.go
@@ -1,1 +1,14 @@
-package singleton
+package outer_singleton
+
+import (
+	"github.com/hitsumabushi/desgin_pattern_golang/05_singleton/singleton"
+	"testing"
+)
+
+func TestSingleton(t *testing.T) {
+	instance_1 := singleton.GetInstance()
+	instance_2 := singleton.GetInstance()
+	if instance_1 != instance_2 {
+		t.Errorf("Get singleton should return always same instance, but this method doesn't.")
+	}
+}
