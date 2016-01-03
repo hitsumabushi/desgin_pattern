798122da1471156fb76da27a09b849bba69261c3
hitsumabushi <053023.math@gmail.com>
2016-01-03 23:15:13 +0900

    Add protptype pattern.

--- 06_prototype/prototype_test.go
+++ 06_prototype/prototype_test.go
@@ -1,1 +1,16 @@
 package prototype
+
+import "testing"
+
+func TestPrototype(t *testing.T) {
+	manager := Manager{map[string]product{}}
+	dash := LinePen{"---"}
+	hat := LinePen{"^^^"}
+	manager.Register("dash", &dash)
+	manager.Register("hat", &hat)
+	clone, err := manager.Create("dash")
+	cloned_dash, ok := clone.(*LinePen)
+	if !ok || cloned_dash == &hat || err != nil {
+		t.Errorf("Create method should return another instance.")
+	}
+}
